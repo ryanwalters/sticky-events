0663a1da955c71d168df04ab9a564f6c53f11160
Ryan Walters <ryan@ryanwalters.dev>
2022-12-01 09:53:45 -0600

    chore: replace rollup-plugin-terser with @rollup/plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const babel = require('@rollup/plugin-babel');
-const { terser } = require('rollup-plugin-terser');
+const terser = require('@rollup/plugin-terser');
 const pkg = require('./package.json');
 
 module.exports = [
